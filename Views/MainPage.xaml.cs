79a8cf8b3c45578478251ed5afaae46c1dedbeac
thesuperfalcon <tintinfalk01@gmail.com>
2024-03-08 13:53:30 +0100

    Added comment

--- Views/MainPage.xaml.cs
+++ Views/MainPage.xaml.cs
@@ -5,7 +5,13 @@ namespace CocktailDBApplication
 {
     public partial class MainPage : ContentPage
     {
-        //Har varken Singleton eller Facade i detta projekt (Skriv förklaring också)
+        /*Har varken Singleton eller Facade i detta projekt då det inte finns någon funktion med att ha dessa design-möster
+         Finns inget syfte med Singleton för att dels så finns det ingen inloggningsfunktion, vilket innebär att en användare behöver inte föras över till olika sidor.
+        Dels så är det att det ända som skickas över till olika sidor är antingen objekten Drink, Ingredient eller en lista på dem.
+        Facade finns det inget syfte då jag inte har någon metod med kedjefunktion som används flera gånger. Enda som skulle kunna göras till en Facade om den används
+        mer än en gång är GetDrinksByIngredientAsync på DrinkViewModel. Den skulle kunna göras om till en Facade om den används flera gånger, men i detta fall används den bara en gång
+        i applikationen.
+         */
         public MainPage()
         {
             InitializeComponent();
